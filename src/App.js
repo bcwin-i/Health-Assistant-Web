2ee8d6a5971e8500922c74d36929939771ddc890
bcwin-i <iziqbek@gmail.com>
2022-01-29 10:55:09 +0000

    First push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from "@mui/styles";
+
+//Local imports
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/background.jpg"})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+  },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const styles = useStyles();
+
+  return <div className={styles.root}></div>;
 }
 
 export default App;
